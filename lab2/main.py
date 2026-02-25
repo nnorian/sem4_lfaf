df86f765c3ceb969dce99b40c5868626ae7c1244
nnorian <kushnirenkoecaterina@gmail.com>
2026-02-25 17:24:24 +0200

    feat: finished the main structure for the 14 th variant

---
+++ lab2/main.py
@@ -0,0 +1,27 @@
+# variant 14
+
+if __name__ == "__main__":
+
+    ndfa = FiniteAutomaton(
+        Q = ['q0', 'q1', 'q2'],
+        sigma = ['a', 'b', 'c'],
+        delta = {
+            ('q0', 'a'): ['q0'],
+            ('q0', 'b'): ['q1'],
+            ('q1', 'c'): ['q1', 'q2'],
+            ('q2', 'a'): ['q0'],
+            ('q1', 'a'): ['q1'],
+        },
+        q0 = 'q0',
+        F = ['q2']
+    )
+
+print(ndfa)
+det = ndfa.is_deterministic()
+
+gram = ndfa.to_regular_grammar()
+print(gram)
+
+dfa = ndfa.to_dfa()
+print(dfa)
+print("{dfa.is_deterministic()}")
